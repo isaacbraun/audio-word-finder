7bf8fa9c24bb56a5bb667081b09a424fb74c282d
Isaac Braun <isaacbraun2000@gmail.com>
2025-03-13 15:36:58 -0700

    feat: add job results summary section

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -2,7 +2,7 @@
 
 use Livewire\Volt\Component;
 use App\Models\Search;
-use Livewire\Attributes\Title;
+use Livewire\Attributes\{Title};
 
 new #[Title('Search Results')] class extends Component {
     public $id;
@@ -26,6 +26,20 @@ new #[Title('Search Results')] class extends Component {
     <flux:heading size="xl" level="1">Results for "{{ $query }}"</flux:heading>
     <flux:subheading>Searching {{ count($files) }} files.</flux:subheading>
 
+    <flux:separator class="my-4" />
+
+    <flux:heading size="lg" level="2">Summary</flux:heading>
+    <div wire:poll.5s>
+        @if ($search->query_total)
+        <flux:heading>{{ $search->query_total }} Matches Found</flux:heading>
+        @else
+        <flux:icon.loading />
+        @endif
+    </div>
+
+    <flux:separator class="my-4" />
+
+    <flux:heading size="lg" level="2">Files</flux:heading>
     <div class="mt-4 flex flex-col gap-4">
         @foreach ($files as $file)
         <livewire:result-card :file="$file" wire:key="{{ $file->id }}" />
