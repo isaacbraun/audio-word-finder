d245747f53c4583f898c7e5711c8b06ef9147887
Isaac Braun <isaacbraun2000@gmail.com>
2025-03-17 10:41:49 -0700

    feat: add parsed_date column to AudioFile

--- app/Models/AudioFile.php
+++ app/Models/AudioFile.php
@@ -8,18 +8,30 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class AudioFile extends Model
 {
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = ['audio_path', 'audio_filename'];
-
-    /**
      * The table associated with the model.
      *
      * @var string
      */
     protected $table = 'files';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['audio_path', 'audio_filename', 'parsed_date'];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'parsed_date' => 'datetime',
+        ];
+    }
 
     /**
      * Get the Search that owns the file
