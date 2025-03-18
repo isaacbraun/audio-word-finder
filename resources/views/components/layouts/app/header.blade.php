635504a83e18b983d215b7a3c81178a5d9802bf6
Isaac Braun <isaacbraun2000@gmail.com>
2025-03-18 16:58:28 -0700

    chore: switch back to header, make it sticky

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -4,7 +4,7 @@
         @include('partials.head')
     </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800">
-        <flux:header container class="border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
+        <flux:header container class="sticky top-0 border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
             <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
 
             <a href="{{ route('home') }}" class="hidden md:flex ml-2 mr-5 items-center space-x-2 lg:ml-0" wire:navigate>
