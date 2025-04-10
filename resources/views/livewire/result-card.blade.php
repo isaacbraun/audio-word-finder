7695b0d554b9317879d632d6da3019940aca9cb6
Isaac Braun <isaacbraun2000@gmail.com>
2025-04-09 19:00:13 -0700

    feat: copy transcription button, reworked card

--- resources/views/livewire/result-card.blade.php
+++ resources/views/livewire/result-card.blade.php
@@ -3,6 +3,8 @@
 use App\Jobs\ProcessFile;
 use App\Models\AudioFile;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 use Livewire\Attributes\Computed;
 use Livewire\Volt\Component;
 
@@ -22,6 +24,12 @@ new class extends Component {
         return $file ?? [];
     }
 
+    #[Computed()]
+    public function badgeString(): string
+    {
+        return $this->file->query_count . ' ' . Str::plural('Match', $this->file->query_count);
+    }
+
     public function retry()
     {
         // Reset transcription path and failed var to show correct UI state
@@ -29,63 +37,85 @@ new class extends Component {
         $this->file->transcription_path = null;
         $this->file->save();
 
-
         ProcessFile::dispatch($this->file->search, $this->file);
+    }
+
+    public function copyTranscription(): void
+    {
+        $fullText = Arr::get($this->transcription(), "fullText");
+        $this->dispatch('copy-to-clipboard', transcription: $fullText);
     }
 
     public function placeholder()
     {
         return <<<'BLADE'
         <div>
-            <flux:card class="h-32 bg-zinc-100/50" />
+            <flux:card class="h-28 bg-zinc-100/50" />
         </div>
         BLADE;
     }
 }; ?>
 
 <div @if (!$this->transcription) wire:poll.2s @endif>
-    <flux:card>
-        <flux:heading>
-            <span class="mr-1 mt-1">{{ $file->audio_filename }}</span>
-
-            @if ($this->file->query_count !== null)
-            <flux:badge size="sm" inset="top bottom" color="{{ $this->file->query_count === 0 ? 'rose' : 'emerald' }}">
-                {{ $this->file->query_count }} Matches
-            </flux:badge>
-            @endif
-        </flux:heading>
-
-        @if ($file->parsed_date)
-        <flux:subheading>{{ $file->parsed_date->toDayDateTimeString() }}</flux:subheading>
-        @endif
-
-
-        <div class="mt-4">
-            @if ($this->transcription)
-            <flux:accordion class="mt-4" variant="reverse">
-                <flux:accordion.item heading="View Transcription">
-                    <flux:accordion.content class="leading-loose">
-                        @foreach ($this->transcription["segments"] as $segment)
-                        @if ($segment["match"])
-                        <flux:badge color="yellow" size="sm" inset="top bottom">{{ $segment["text"] }}</flux:badge>
+    <flux:card class="!p-4">
+        <div class="flex flex-row flex-wrap gap-2 justify-between">
+            <div>
+                <flux:heading>
+                    <span class="break-all mr-1">
+                        @if ($file->parsed_date)
+                        {{ $file->parsed_date->toDayDateTimeString() }}
                         @else
-                        <span wire:key="$loop->index">{{ $segment["text"] }}</span>
+                        {{ $file->audio_filename }}
                         @endif
-                        @endforeach
-                    </flux:accordion.content>
-                </flux:accordion.item>
-            </flux:accordion>
-            @elseif ($this->failed)
-            <flux:callout icon="exclamation-triangle" color="red" inline>
-                <flux:callout.heading>Processing failed</flux:callout.heading>
+                    </span>
 
-                <x-slot name="actions">
-                    <flux:button wire:click="retry">Retry</flux:button>
-                </x-slot>
-            </flux:callout>
-            @else
+                    @if ($this->file->query_count !== null)
+                    <flux:badge size="sm" inset="top bottom" color="{{ $this->file->query_count === 0 ? 'rose' : 'emerald' }}">
+                        {{ $this->file->query_count }} Matches
+                    </flux:badge>
+                    @endif
+                </flux:heading>
+
+                @if ($file->parsed_date)
+                <flux:text class="mt-1 break-all">{{ $file->audio_filename }}</flux:text>
+                @endif
+            </div>
+
+            @if (!$this->transcription)
             <flux:icon.loading variant="micro" />
+            @elseif (Arr::has($this->transcription, "fullText"))
+            <flux:button
+                wire:click="copyTranscription"
+                icon:trailing="document-duplicate"
+                variant="subtle"
+                size="sm"
+                title="Copy transcription to clipboard"
+                inset></flux:button>
             @endif
         </div>
+
+        @if ($this->transcription)
+        <flux:accordion class="mt-2" variant="reverse">
+            <flux:accordion.item heading="View Transcription">
+                <flux:accordion.content class="leading-loose">
+                    @foreach ($this->transcription["segments"] as $segment)
+                    @if ($segment["match"])
+                    <flux:badge color="yellow" size="sm" inset="top bottom">{{ $segment["text"] }}</flux:badge>
+                    @else
+                    <span wire:key="$loop->index">{{ $segment["text"] }}</span>
+                    @endif
+                    @endforeach
+                </flux:accordion.content>
+            </flux:accordion.item>
+        </flux:accordion>
+        @elseif ($this->failed)
+        <flux:callout class="mt-2" icon="exclamation-triangle" color="red" inline>
+            <flux:callout.heading>Processing failed</flux:callout.heading>
+
+            <x-slot name="actions">
+                <flux:button wire:click="retry">Retry</flux:button>
+            </x-slot>
+        </flux:callout>
+        @endif
     </flux:card>
 </div>
