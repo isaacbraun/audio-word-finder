2e27fc05b38f94a3f3354f36b19bec4cbb31802a
Isaac Braun <isaacbraun2000@gmail.com>
2025-04-10 18:36:36 -0700

    feat: add upgrade link, hide history if not subscribed

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -1,101 +1,114 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark scroll-smooth">
-    <head>
-        @include('partials.head')
-    </head>
-    <body class="min-h-screen bg-white dark:bg-zinc-800">
-        <nav aria-label="Skip links">
-            <ul>
-                <li><a href="#navigation" class="sr-only" title="Skip to Navigation">Skip to Navigation</a></li>
-                <li><a href="#content" class="sr-only" title="Skip to Content">Skip to Content</a></li>
-            </ul>
-        </nav>
 
-        <flux:header id="navigation" container class="sticky top-0 border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
-            <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
+<head>
+    @include('partials.head')
+</head>
 
-            <a href="{{ route('home') }}" class="hidden md:flex ml-2 mr-5 items-center space-x-2 lg:ml-0" wire:navigate.hover>
-                <x-app-logo />
-            </a>
+<body class="min-h-screen bg-white dark:bg-zinc-800">
+    <nav aria-label="Skip links">
+        <ul>
+            <li><a href="#navigation" class="sr-only" title="Skip to Navigation">Skip to Navigation</a></li>
+            <li><a href="#content" class="sr-only" title="Skip to Content">Skip to Content</a></li>
+        </ul>
+    </nav>
 
-            <flux:navbar class="-mb-px max-lg:hidden">
-                <flux:navbar.item icon="plus" :href="route('new')" :current="request()->routeIs('new')" wire:navigate.hover>
-                    {{ __('New Search') }}
-                </flux:navbar.item>
-                <flux:navbar.item icon="clock" :href="route('history')" :current="request()->routeIs('history')" wire:navigate.hover>
-                    {{ __('History') }}
-                </flux:navbar.item>
-            </flux:navbar>
+    <flux:header id="navigation" container class="sticky top-0 border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
+        <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
 
-            <flux:spacer />
+        <a href="{{ route('home') }}" class="hidden md:flex ml-2 mr-5 items-center space-x-2 lg:ml-0" wire:navigate.hover>
+            <x-app-logo />
+        </a>
 
-            <!-- Desktop User Menu -->
-            <flux:dropdown position="top" align="end">
-                <flux:profile
-                    class="cursor-pointer"
-                    :initials="auth()->user()->initials()"
-                />
+        <flux:navbar class="-mb-px max-lg:hidden">
+            <flux:navbar.item icon="plus" :href="route('new')" :current="request()->routeIs('new')" wire:navigate.hover>
+                {{ __('New Search') }}
+            </flux:navbar.item>
+            @if (Auth::user()->subscribed())
+            <flux:navbar.item icon="clock" :href="route('history')" :current="request()->routeIs('history')" wire:navigate.hover>
+                {{ __('History') }}
+            </flux:navbar.item>
+            @else
+            <flux:navbar.item icon="currency-dollar" :href="route('subscribe-basic')">
+                {{ __('Upgrade') }}
+            </flux:navbar.item>
+            @endif
+        </flux:navbar>
 
-                <flux:menu>
-                    <flux:menu.radio.group>
-                        <div class="p-0 text-sm font-normal">
-                            <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                                <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
-                                    <span
-                                        class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white"
-                                    >
-                                        {{ auth()->user()->initials() }}
-                                    </span>
+        <flux:spacer />
+
+        <!-- Desktop User Menu -->
+        <flux:dropdown position="top" align="end">
+            <flux:profile
+                class="cursor-pointer"
+                :initials="auth()->user()->initials()" />
+
+            <flux:menu>
+                <flux:menu.radio.group>
+                    <div class="p-0 text-sm font-normal">
+                        <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                            <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
+                                <span
+                                    class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
+                                    {{ auth()->user()->initials() }}
                                 </span>
+                            </span>
 
-                                <div class="grid flex-1 text-left text-sm leading-tight">
-                                    <span class="truncate font-semibold">{{ auth()->user()->name }}</span>
-                                    <span class="truncate text-xs">{{ auth()->user()->email }}</span>
-                                </div>
+                            <div class="grid flex-1 text-left text-sm leading-tight">
+                                <span class="truncate font-semibold">{{ auth()->user()->name }}</span>
+                                <span class="truncate text-xs">{{ auth()->user()->email }}</span>
                             </div>
                         </div>
-                    </flux:menu.radio.group>
+                    </div>
+                </flux:menu.radio.group>
 
-                    <flux:menu.separator />
+                <flux:menu.separator />
 
-                    <flux:menu.radio.group>
-                        <flux:menu.item href="/settings/profile" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.item>
-                    </flux:menu.radio.group>
+                <flux:menu.radio.group>
+                    <flux:menu.item href="/settings/profile" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.item>
+                </flux:menu.radio.group>
 
-                    <flux:menu.separator />
+                <flux:menu.separator />
 
-                    <form method="POST" action="{{ route('logout') }}" class="w-full">
-                        @csrf
-                        <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full">
-                            {{ __('Log Out') }}
-                        </flux:menu.item>
-                    </form>
-                </flux:menu>
-            </flux:dropdown>
-        </flux:header>
+                <form method="POST" action="{{ route('logout') }}" class="w-full">
+                    @csrf
+                    <flux:menu.item as="button" type="submit" icon="arrow-right-start-on-rectangle" class="w-full">
+                        {{ __('Log Out') }}
+                    </flux:menu.item>
+                </form>
+            </flux:menu>
+        </flux:dropdown>
+    </flux:header>
 
-        <!-- Mobile Menu -->
-        <flux:sidebar stashable sticky class="lg:hidden border-r border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
-            <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
+    <!-- Mobile Menu -->
+    <flux:sidebar stashable sticky class="lg:hidden border-r border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
+        <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
 
-            <a href="{{ route('home') }}" class="ml-1 flex items-center space-x-2" wire:navigate>
-                <x-app-logo />
-            </a>
+        <a href="{{ route('home') }}" class="ml-1 flex items-center space-x-2" wire:navigate>
+            <x-app-logo />
+        </a>
 
-            <flux:navlist variant="outline">
-                <flux:navlist.group :heading="__('Platform')">
-                    <flux:navlist.item icon="plus" :href="route('new')" :current="request()->routeIs('dashboard')" wire:navigate>
+        <flux:navlist variant="outline">
+            <flux:navlist.group :heading="__('Platform')">
+                <flux:navlist.item icon="plus" :href="route('new')" :current="request()->routeIs('dashboard')" wire:navigate>
                     {{ __('New Search') }}
-                    </flux:navlist.item>
+                </flux:navlist.item>
+                @if (Auth::user()->subscribed())
                 <flux:navbar.item icon="clock" :href="route('history')" :current="request()->routeIs('history')" wire:navigate>
                     {{ __('History') }}
                 </flux:navbar.item>
-                </flux:navlist.group>
-            </flux:navlist>
-        </flux:sidebar>
+                @else
+                <flux:navbar.item icon="currency-dollar" :href="route('subscribe-basic')">
+                    {{ __('Upgrade') }}
+                </flux:navbar.item>
+                @endif
+            </flux:navlist.group>
+        </flux:navlist>
+    </flux:sidebar>
 
-        {{ $slot }}
+    {{ $slot }}
 
-        @fluxScripts
-    </body>
+    @fluxScripts
+</body>
+
 </html>
