e51c9129a0c48ffe447e6723a16da0248acf490b
Isaac Braun <isaacbraun2000@gmail.com>
2025-04-13 19:11:11 -0700

    chore: try to make messaging clearer and button not as predominant

--- resources/views/livewire/auth/verify-email.blade.php
+++ resources/views/livewire/auth/verify-email.blade.php
@@ -35,9 +35,9 @@ new #[Layout('components.layouts.auth')] class extends Component {
 }; ?>
 
 <div class="mt-4 flex flex-col gap-6">
-    <flux:text class="text-center">
+    <flux:heading size="lg" class="text-center">
         {{ __('Please verify your email address by clicking on the link we just emailed to you.') }}
-    </flux:text>
+    </flux:heading>
 
     @if (session('status') == 'verification-link-sent')
         <flux:text class="text-center font-medium !dark:text-green-400 !text-green-600">
@@ -46,7 +46,7 @@ new #[Layout('components.layouts.auth')] class extends Component {
     @endif
 
     <div class="flex flex-col items-center justify-between space-y-3">
-        <flux:button wire:click="sendVerification" variant="primary" class="w-full">
+        <flux:button wire:click="sendVerification">
             {{ __('Resend verification email') }}
         </flux:button>
 
