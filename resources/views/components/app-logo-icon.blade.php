7813cf81e8afaff5a6b0af90ebaffb8e71e75f29
Isaac Braun <isaacbraun2000@gmail.com>
2025-04-30 13:21:52 -0700

    chore: move icon styles to header, icon used elsewhere

--- resources/views/components/app-logo-icon.blade.php
+++ resources/views/components/app-logo-icon.blade.php
@@ -1,3 +1,1 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <flux:icon.speaker-wave  {{ $attributes }} />
-</div>
+<flux:icon.speaker-wave {{ $attributes }} />
