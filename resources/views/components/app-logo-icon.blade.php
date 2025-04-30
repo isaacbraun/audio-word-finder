76e5ce340501cfe2e60feaa4b2c4bda517b12595
Isaac Braun <isaacbraun2000@gmail.com>
2025-04-30 12:50:00 -0700

    chore: move icon into its own component

--- resources/views/components/app-logo-icon.blade.php
+++ resources/views/components/app-logo-icon.blade.php
@@ -1,1 +1,3 @@
-<flux:icon.speaker-wave {{ $attributes }} />
+<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
+    <flux:icon.speaker-wave  {{ $attributes }} />
+</div>
--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,4 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <flux:icon.speaker-wave />
-</div>
+<x-app-logo-icon />
 <div class="ml-1 grid flex-1 text-left text-sm">
     <span class="mb-0.5 truncate leading-none font-semibold">Audio Word Finder</span>
 </div>
