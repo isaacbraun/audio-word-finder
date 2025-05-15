e96e25caa21696fd6952a82f186f1de3834e7bc9
Isaac Braun <isaacbraun2000@gmail.com>
2025-05-15 12:55:45 -0700

    chore: r2 fixes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'stripe/*',
         ]);
-        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
