fad8f6b0571cce8b65d960ce670eed74b0c5f311
Isaac Braun <isaacbraun2000@gmail.com>
2025-05-14 17:19:34 -0700

    fix: trust all proxies for cloudflare/coolify

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'stripe/*',
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
